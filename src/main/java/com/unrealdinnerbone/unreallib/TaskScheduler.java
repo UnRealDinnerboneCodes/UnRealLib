867514ef391369fe8c6bb12447669da9d95b5157
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2020-10-04 20:03:59 -0500

    Fixed another error

--- src/main/java/com/unrealdinnerbone/unreallib/TaskScheduler.java
+++ src/main/java/com/unrealdinnerbone/unreallib/TaskScheduler.java
@@ -1,30 +1,11 @@
 package com.unrealdinnerbone.unreallib;
 
-
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Marker;
 
 import java.util.concurrent.*;
 
 public class TaskScheduler {
-
-//    private static final ScheduledExecutorService executor  = Executors.newScheduledThreadPool(Math.max(1, Runtime.getRuntime().availableProcessors() - 1), new ThreadFactoryBuilder().setNameFormat("scheduler-%d").setUncaughtExceptionHandler(new TaskExceptionHandler()).build());
-//
-//    public static void scheduleAtFixedRate(int time, TimeUnit timeUnit, Runnable runnable) {
-//        executor.scheduleAtFixedRate(runnable, 0, time, timeUnit);
-//    }
-//    public static void handleTaskOnThread(int time, TimeUnit timeUnit, Runnable runnable) {
-//        executor.schedule(runnable, time, timeUnit);
-//    }
-//
-//    @Slf4j
-//    public static class TaskExceptionHandler implements Thread.UncaughtExceptionHandler {
-//        @Override
-//        public void uncaughtException(Thread t, Throwable e) {
-//            log.error("Error while handling task on thread " + t.getName(), e);
-//        }
-//    }
 
     private static ScheduledExecutorService taskExecutor;
     private static ExecutorService executorService = newCachedThreadPool();
