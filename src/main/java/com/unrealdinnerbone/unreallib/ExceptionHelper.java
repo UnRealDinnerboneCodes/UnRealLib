b3deb8d3718905d9b19111f0cfebd0ccab469fc9
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2021-12-13 19:43:18 -0600

    Change how it worked

--- src/main/java/com/unrealdinnerbone/unreallib/ExceptionHelper.java
+++ src/main/java/com/unrealdinnerbone/unreallib/ExceptionHelper.java
@@ -3,9 +3,17 @@ package com.unrealdinnerbone.unreallib;
 import com.unrealdinnerbone.unreallib.exception.ExceptionConsumer;
 import com.unrealdinnerbone.unreallib.exception.ExceptionSuppler;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class ExceptionHelper {
 
-    public <T, E extends Exception> void handle(ExceptionSuppler<T, E> suppler, ExceptionConsumer<T, E> exceptionConsumer) throws Exception {
-        exceptionConsumer.accept(suppler.get());
+    public <T, E extends Exception> void handle(ExceptionSuppler<T, E> suppler, ExceptionConsumer<T, E> exceptionConsumer, Consumer<E>[] fails) {
+        try {
+            exceptionConsumer.accept(suppler.get());
+        }catch(Exception e){
+            Arrays.stream(fails).forEach(fail -> fail.accept((E) e));
+        }
+
     }
 }
