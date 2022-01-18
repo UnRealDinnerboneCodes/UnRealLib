feed33df6f7f8889ff1b3c7adb22e55856aed365
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-01-18 11:12:11 -0600

    Maps

---
+++ src/main/java/com/unrealdinnerbone/unreallib/Maps.java
@@ -0,0 +1,13 @@
+package com.unrealdinnerbone.unreallib;
+
+import java.util.Map;
+
+public class Maps
+{
+    public static <K, V> V putIfAbsent(Map<K, V> map, K key, V value) {
+        if(!map.containsKey(key)) {
+            map.put(key, value);
+        }
+        return map.get(key);
+    }
+}
