88b2d875eaa656c6e25d6086f171e0073f30e854
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2023-04-04 21:11:43 -0500

    Check for null

--- src/gson/java/com/unrealdinnerbone/unreallib/json/gson/parsers/basic/InstantAdapter.java
+++ src/gson/java/com/unrealdinnerbone/unreallib/json/gson/parsers/basic/InstantAdapter.java
@@ -2,6 +2,7 @@ package com.unrealdinnerbone.unreallib.json.gson.parsers.basic;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
@@ -20,7 +21,11 @@ public class InstantAdapter extends TypeAdapter<Instant> {
 
     @Override
     public Instant read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
         String s = in.nextString();
-        return Instant.parse(s);
+        return s == null || s.isEmpty() || s.isBlank() ? null : Instant.parse(s);
     }
 }
